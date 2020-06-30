b2945fd9b8b3b1b6bac3bd2c7b57a5d5ffca5b7e
Keren <keren@aratours.com>
2020-06-29 23:33:42 -0600

    validate cedula

---
+++ js/index.js
@@ -0,0 +1,22 @@
+function validation(){
+
+    var numCedula= document.getElementById('ced'),
+        format = /^[1-9]-\d{4}-\d{4}$/;
+        // Creamos el elemento p, dinámicamente, para colocar el mensaje de error de cedula invalida
+    var newDiv = document.createElement('p');
+    var content=newDiv.appendChild(document.createTextNode('Cédula con formato inválido'));
+
+    if (format.test(numCedula.value)) 
+    {
+        location.href="./view/products.html";
+    } else 
+    {
+        numCedula.style.cssText='border: 2px solid red;border-radius: 5px;padding: 5px;';
+
+        // Div de referencia para saber debajo de cual elemento se va a agreagar
+        var ref = document.getElementById('ced').getElementsByTagName('input')[0];
+        // Y ahora lo agregamos al DOM
+        document.getElementById('opt').insertBefore(content,ref);
+        setTimeout(()=>document.getElementById('opt').removeChild(content), 10000); // Elimina el elemento despues de 10 segundos
+    }
+}
